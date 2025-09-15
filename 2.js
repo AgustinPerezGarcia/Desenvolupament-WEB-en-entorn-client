ce9ad07a759c8f980da8a35edf53cf128ec6f760
Agustin Perez Garcia <agupergar@alu.edu.gva.es>
2025-09-15 16:40:26 +0200

    Ejercicio 2

---
+++ 2.js
@@ -0,0 +1,14 @@
+let fila = parseFloat(prompt("Cuantas filas quieres:"))
+let columna = parseFloat(prompt("Cuantas columnas quieres:"))
+
+for (let index = 0; index < columna; index++) {
+    document.write("<td>")
+
+    for (let index = 0; index < fila; index++) {
+        document.write("<tr>Agustin</tr>")        
+    
+    }
+    
+    document.write("</td>")
+    
+}
